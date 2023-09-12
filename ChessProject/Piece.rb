dcbf600bc8abae10628802d62ab34c193f9cb21c
Stephen Chao <stephen.liu.chao@protonmail.com>
2023-09-12 14:25:15 -0700

    pawned

--- ChessProject/Piece.rb
+++ ChessProject/Piece.rb
@@ -1,9 +1,61 @@
 require_relative "Board"
 
 class Piece
-    def initialize(pos)
+    attr_reader :pos
+    def initialize(board,color,pos)
+        @board=board
+        @color=color
         @pos=pos
     end
-
+    def to_s
+    end
+    def empty?
+    end
+    def valid_moves
+    end
+    def pos=(val)
+    end
+    def Symbol
+    end
+    private
+    def move_into_check?(end_pos)
+    end
     
+end
+
+class Pawn < Piece
+    def symbol
+
+    end
+
+    def moves
+
+    end
+
+    private
+    def at_start_row?
+        if row == 1 or row == 6
+            return true
+        end
+        false
+    end
+    def forward_dir
+        row,col = self.pos
+        if pos[row+1][col]==nil
+            return 1
+        else
+            return -1
+        end
+        
+    end
+    def forward_steps
+    end
+    def side_attacks
+        row,col=self.pos
+        if pos[row+1][col+1]!= nil
+            return 1
+        else
+            return -1
+        end
+    end
 end
