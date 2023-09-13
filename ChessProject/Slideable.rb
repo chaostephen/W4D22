578463e88ec0a9a245d5fc638ce41c3fc94f3e98
Stephen Chao <stephen.liu.chao@protonmail.com>
2023-09-13 08:29:37 -0700

    working on slidable2

--- ChessProject/Slideable.rb
+++ ChessProject/Slideable.rb
@@ -1,15 +1,29 @@
+require_relative "Board"
 module Slideable
-
+    attr_reader :HORIZONTAL_DIRS, :DIAGONAL_DIRS
     def valid_moves?(pos)
         result = []
         HORIZONTAL_DIRS =[
             [0,1],[0,-1],[1,0],[-1,0]
     ].freeze
-        end
         DIAGONAL_DIRS =[
             [1.1],[-1,-1],[1,-1],[-1,1]
     ].freeze
+    end
+    def moves
+
+        all_moves=[]
 
     end
+    private
+    def move_dirs
+        raise NotImplementedError if valid_moves ==false
+
+    end
+    def grow_unblocked_moves_in_dir(dx,dy)
+        
+    end
+    
+
 
 end
