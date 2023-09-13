93ef0df2c503eca7904d20278e54ffe787acc1c1
Allon Nam <allon.nam96@gmail.com>
2023-09-13 16:01:48 -0400

    slideabless

--- ChessProject/Slideable.rb
+++ ChessProject/Slideable.rb
@@ -1,4 +1,5 @@
 require_relative "Board"
+
 module Slideable
     attr_reader :HORIZONTAL_DIRS, :DIAGONAL_DIRS
     def valid_moves?(pos)
@@ -22,15 +23,43 @@ module Slideable
     end
     private
     def move_dirs
-        raise NotImplementedError if valid_moves ==false
+        raise NotImplementedError 
 
     end
     def grow_unblocked_moves_in_dir(dx,dy)
-        # if the board is not nil, move the piece to that position
-        if moves.each do |move|
-            move
-            # move to the opposite color piece
-        end
+        moves = []
+    # create an array to collect moves
+
+    # get the piece's current row and current column
+   row, col = self.pos
+    counter = 2
+    while row < 7 && row > 0 && col < 7 && col > 0
+    if Board[row+dx, col+dy].nil?
+        moves << [row+dx, col+dy] 
+    dx * counter
+    dy * counter
+    elsif !Board[row+dx, col+dy].nil?
+    counter += 1
+    end
+    moves
+
+
+    # in a loop:
+      # continually increment the piece's current row and current column to generate a new position
+      # stop looping if the new position is invalid (not on the board); the piece can't move in this direction
+      # if the new position is empty, the piece can move here, so add the new position to the moves array
+      # if the new position is occupied with a piece of the opposite color, the piece can move here (to capture the opposing piece), so add the new position to the moves array
+        # but, the piece cannot continue to move past this piece, so stop looping
+      # if the new position is occupied with a piece of the same color, stop looping
+
+    # return the final moves array
+
+    # count += 1
+    # HORIZONTAL_DIRS.each do |dir|
+    #     if self.move_dirs.dir == true
+    #         counter += 1
+    #         dir * counter
+    #     end
         end
     end
     
